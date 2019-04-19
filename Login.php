477190e1bdc98c3063a911428c7cc966159fefb7
Richard-Williams-97 <49793505+Richard-Williams-97@users.noreply.github.com>
2019-04-19 15:15:52 -0400

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,353 @@
+
+
+<html lang="en" class="fixed-header"><head>
+	<title>Home - Georgia Youth Challenge</title>
+	<meta charset="utf-8">
+	<meta name="Description" content="The National Guard Youth ChalleNGe Program was established by Congress in 1993 to turn around the lives of young men and women between the ages of 16 and 18 who are experiencing difficulty in completing traditional high school. This is a free program open to Georgia residents.">
+	<meta name="Keywords" content="National guard youth challenge program, ga youth challenge experience">
+	
+		<meta name="generator" content="6.3.455">
+	
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Third Wave Digital - www.thirdwavedigital.com">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0, shrink-to-fit=no">
+	<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+	<link href="//fonts.googleapis.com/css?family=Poppins:300,400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="/admin/assets/pkg/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/vendor/font-awesome.min.css">
+	<link rel="stylesheet" href="/admin/assets/css/animate.css">
+	<!-- Homepage Start -->
+	<link rel="stylesheet" href="/js/vendor/slick/slick.css">
+	<!-- Homepage End -->
+    <link rel="stylesheet" href="/css/main.css">
+	
+		<script async="" src="//www.google-analytics.com/analytics.js"></script><script src="/admin/assets/js/vendor/jquery-1.11.0.min.js"></script>
+		<script src="/admin/assets/pkg/bootstrap/js/bootstrap.min.js"></script>
+	
+	<!-- Homepage Start -->
+	<script src="/js/vendor/slick/slick.custom.js"></script>
+		<script src="/admin/assets/js/vendor/jquery.matchHeight-min.js"></script>
+	<!-- Homepage End -->
+    <script src="/js/main.js"></script>
+
+					<script>
+					(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+   ga('create', 'UA-55387456-1', 'auto');
+   ga('send', 'pageview');
+					</script>
+				
+<script charset="utf-8" src="https://platform.twitter.com/js/moment~timeline~tweet.6e5b62723488aee38af0c77681396a5b.js"></script><script charset="utf-8" src="https://platform.twitter.com/js/timeline.4c6ab682148a0366f9efb1647a3f4799.js"></script></head> 
+<body style="">
+
+	<a href="#maincontent" class="skip">Skip to main content</a>
+
+	<!--[if lte IE 9]>
+   		<div class="alert alert-danger alert-dismissible browser-alert">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</div>
+	<![endif]-->
+
+	<section class="container-full search-wrapper" role="search" id="search" aria-hidden="true">
+		<form class="container" method="post" action="/search.cms">
+			<label for="keywords1" class="sr-only">Site Search</label>
+			<input type="text" name="keywords" id="keywords1" placeholder="Search ..." tabindex="-1">
+			<button type="submit" class="btn" aria-label="search" tabindex="-1"><span class="fa fa-search" aria-hidden="true"></span></button>
+		</form>
+	</section><!--/ .search-wrapper-->
+
+	<section class="container-full header-wrapper" role="banner">
+    	<header class="container">
+			<a href="/" class="logomark"><img src="/img/gyc-logo.png" alt="Georgia Youth Challenge"></a>
+			<p class="logotype hidden-xxs" role="heading">Georgia Youth ChalleNGe Program</p><!-- Additional .hidden class here for this template only -->
+			<nav>
+				<ul class="quicklinks">
+					<li><a href="https://www.facebook.com/GeorgiaYCA/" target="_blank"><span class="fa fa-facebook" aria-hidden="true"></span><span class="sr-only">Facebook</span></a></li>
+					<li><a href="https://twitter.com/georgiayca" target="_blank"><span class="fa fa-twitter" aria-hidden="true"></span><span class="sr-only">Twitter</span></a></li>
+					<li><a href="https://www.instagram.com/georgiayca/" target="_blank"><span class="fa fa-instagram" aria-hidden="true"></span><span class="sr-only">Instagram</span></a></li>
+	    			<li><button type="button" class="btn btn-transparent btn-showsearch" aria-controls="search" aria-label="Search"><span class="fa fa-search fa-fw" aria-hidden="true"></span></button></li>
+					<li class="text-uppercase"><a href="/contact-us/careers.cms">Careers</a></li>
+					<li class="text-uppercase"><a href="/contact-us/request-info.cms">Contact</a></li>
+	    		</ul>
+    			<ul class="mainmenu">
+					
+						<li><a href="/about-us/mission-and-values.cms" aria-haspopup="true">About Us </a><button type="button" class="btn btn-toggle-dropdown btn-default hidden-sm hidden-md hidden-lg" aria-label="Toggle Dropdown"><span class="fa fa-angle-down" aria-hidden="true"></span></button>
+							<ul aria-hidden="true" aria-label="submenu">
+								
+									<li><a href="/about-us/mission-and-values.cms"> Mission and Values</a></li>
+								
+									<li><a href="/about-us/challenge-process.cms"> The ChalleNGe Process</a></li>
+								
+									<li><a href="/about-us/eight-core-components.cms"> Eight Core Components </a></li>
+								
+									<li><a href="/about-us/success-stories.cms"> Success Stories</a></li>
+								
+									<li><a href="/about-us/videos.cms"> Videos</a></li>
+								
+									<li><a href="/about-us/calendar-events-news.cms"> Calendar</a></li>
+								
+							</ul>
+						</li>
+					
+						<li><a href="/academies/programs-in-georgia.cms" aria-haspopup="true">Academies </a><button type="button" class="btn btn-toggle-dropdown btn-default hidden-sm hidden-md hidden-lg" aria-label="Toggle Dropdown"><span class="fa fa-angle-down" aria-hidden="true"></span></button>
+							<ul aria-hidden="true" aria-label="submenu">
+								
+									<li><a href="/academies/programs-in-georgia.cms"> Programs in Georgia</a></li>
+								
+									<li><a href="/academies/fort-stewart/"> Fort Stewart</a></li>
+								
+									<li><a href="/academies/fort-gordon/"> Fort Gordon</a></li>
+								
+									<li><a href="/academies/milledgeville/"> Milledgeville</a></li>
+								
+							</ul>
+						</li>
+					
+						<li><a href="/admissions/recruitment.cms" aria-haspopup="true">Admissions </a><button type="button" class="btn btn-toggle-dropdown btn-default hidden-sm hidden-md hidden-lg" aria-label="Toggle Dropdown"><span class="fa fa-angle-down" aria-hidden="true"></span></button>
+							<ul aria-hidden="true" aria-label="submenu">
+								
+									<li><a href="/admissions/recruitment.cms"> Recruitment Overview</a></li>
+								
+									<li><a href="/admissions/application.cms"> Application</a></li>
+								
+									<li><a href="/admissions/required-documents.cms"> Required Documents</a></li>
+								
+									<li><a href="/admissions/how-to-prepare-for-yca.cms"> How to Prepare for YCA</a></li>
+								
+									<li><a href="/admissions/faqs.cms"> FAQs</a></li>
+								
+							</ul>
+						</li>
+					
+	    			<li class="resources"><a href="/resources-for/parents-and-guardians.cms" class="btn btn-black-gradient" aria-haspopup="true"><span class="fa fa-user-circle" aria-hidden="true"></span> Resources For</a> <button type="button" class="btn btn-toggle-dropdown btn-default hidden-sm hidden-md hidden-lg" aria-label="Toggle Dropdown"><span class="fa fa-angle-down" aria-hidden="true"></span></button>
+						<ul aria-hidden="true" aria-label="submenu">
+						
+							<li><a href="/resources-for/parents-and-guardians.cms">Parents and Guardians</a></li>
+						
+							<li><a href="/resources-for/high-school-counselors.cms">High School Counselors</a></li>
+						
+							<li><a href="/resources-for/juvenile-justice.cms">Juvenile Justice</a></li>
+						
+							<li><a href="/resources-for/mentors.cms">Mentors</a></li>
+						
+						</ul>
+					</li>
+	    			<li><a href="/admissions/application.cms" class="btn btn-orange-gradient"><span class="fa fa-pencil-square-o" aria-hidden="true"></span> Apply</a></li>
+	    		</ul>
+	    		<button type="button" class="btn btn-toggle-offcanvas hidden-sm hidden-md hidden-lg" aria-controls="offcanvasmenu">Menu <span class="fa fa-bars" aria-hidden="true"></span></button>
+    		</nav>
+		</header>
+	</section><!--/ .header-wrapper-->
+
+	<nav class="offcanvasmenu" aria-label="Offcanvas Menu" id="offcanvasmenu" aria-hidden="true" tabindex="-1">
+		<div class="bg-grey">
+			<form method="post" action="/search.cms">
+				<label for="keywords2" class="sr-only">Search</label>
+				<input type="text" name="keywords" id="keywords2" placeholder="Search ...">
+				<button type="submit" class="btn btn-transparent" aria-label="search"><span class="fa fa-search" aria-hidden="true"></span></button>
+			</form>
+		</div><ul class="mainmenu">
+					
+						<li class="has-ul"><a href="/about-us/mission-and-values.cms" aria-haspopup="true">About Us </a><button type="button" class="btn btn-toggle-dropdown btn-default hidden-sm hidden-md hidden-lg" aria-label="Toggle Dropdown"><span class="fa fa-angle-down" aria-hidden="true"></span></button>
+							<ul aria-hidden="true" aria-label="submenu">
+								
+									<li><a href="/about-us/mission-and-values.cms"> Mission and Values</a></li>
+								
+									<li><a href="/about-us/challenge-process.cms"> The ChalleNGe Process</a></li>
+								
+									<li><a href="/about-us/eight-core-components.cms"> Eight Core Components </a></li>
+								
+									<li><a href="/about-us/success-stories.cms"> Success Stories</a></li>
+								
+									<li><a href="/about-us/videos.cms"> Videos</a></li>
+								
+									<li><a href="/about-us/calendar-events-news.cms"> Calendar</a></li>
+								
+							</ul>
+						</li>
+					
+						<li class="has-ul"><a href="/academies/programs-in-georgia.cms" aria-haspopup="true">Academies </a><button type="button" class="btn btn-toggle-dropdown btn-default hidden-sm hidden-md hidden-lg" aria-label="Toggle Dropdown"><span class="fa fa-angle-down" aria-hidden="true"></span></button>
+							<ul aria-hidden="true" aria-label="submenu">
+								
+									<li><a href="/academies/programs-in-georgia.cms"> Programs in Georgia</a></li>
+								
+									<li><a href="/academies/fort-stewart/"> Fort Stewart</a></li>
+								
+									<li><a href="/academies/fort-gordon/"> Fort Gordon</a></li>
+								
+									<li><a href="/academies/milledgeville/"> Milledgeville</a></li>
+								
+							</ul>
+						</li>
+					
+						<li class="has-ul"><a href="/admissions/recruitment.cms" aria-haspopup="true">Admissions </a><button type="button" class="btn btn-toggle-dropdown btn-default hidden-sm hidden-md hidden-lg" aria-label="Toggle Dropdown"><span class="fa fa-angle-down" aria-hidden="true"></span></button>
+							<ul aria-hidden="true" aria-label="submenu">
+								
+									<li><a href="/admissions/recruitment.cms"> Recruitment Overview</a></li>
+								
+									<li><a href="/admissions/application.cms"> Application</a></li>
+								
+									<li><a href="/admissions/required-documents.cms"> Required Documents</a></li>
+								
+									<li><a href="/admissions/how-to-prepare-for-yca.cms"> How to Prepare for YCA</a></li>
+								
+									<li><a href="/admissions/faqs.cms"> FAQs</a></li>
+								
+							</ul>
+						</li>
+					
+	    			<li class="resources has-ul"><a href="/resources-for/parents-and-guardians.cms" class="btn btn-black-gradient" aria-haspopup="true"><span class="fa fa-user-circle" aria-hidden="true"></span> Resources For</a> <button type="button" class="btn btn-toggle-dropdown btn-default hidden-sm hidden-md hidden-lg" aria-label="Toggle Dropdown"><span class="fa fa-angle-down" aria-hidden="true"></span></button>
+						<ul aria-hidden="true" aria-label="submenu">
+						
+							<li><a href="/resources-for/parents-and-guardians.cms">Parents and Guardians</a></li>
+						
+							<li><a href="/resources-for/high-school-counselors.cms">High School Counselors</a></li>
+						
+							<li><a href="/resources-for/juvenile-justice.cms">Juvenile Justice</a></li>
+						
+							<li><a href="/resources-for/mentors.cms">Mentors</a></li>
+						
+						</ul>
+					</li>
+	    			<li><a href="/admissions/application.cms" class="btn btn-orange-gradient"><span class="fa fa-pencil-square-o" aria-hidden="true"></span> Apply</a></li>
+	    		</ul>
+	<ul class="quicklinks">
+					<li><a href="https://www.facebook.com/GeorgiaYCA/" target="_blank"><span class="fa fa-facebook" aria-hidden="true"></span><span class="sr-only">Facebook</span></a></li>
+					<li><a href="https://twitter.com/georgiayca" target="_blank"><span class="fa fa-twitter" aria-hidden="true"></span><span class="sr-only">Twitter</span></a></li>
+					<li><a href="https://www.instagram.com/georgiayca/" target="_blank"><span class="fa fa-instagram" aria-hidden="true"></span><span class="sr-only">Instagram</span></a></li>
+	    			<li><button type="button" class="btn btn-transparent btn-showsearch" aria-controls="search" aria-label="Search"><span class="fa fa-search fa-fw" aria-hidden="true"></span></button></li>
+					<li class="text-uppercase"><a href="/contact-us/careers.cms">Careers</a></li>
+					<li class="text-uppercase"><a href="/contact-us/request-info.cms">Contact</a></li>
+	    		</ul></nav><!--/ .offcanvas-menu-->
+
+	<main id="maincontent">
+
+	
+
+<!-- Login form -->
+<div>
+	<h2>Superuser login</h2>
+ <div class = "container">
+  <form action ="./loginHandler.php" method = "POST">
+  email:<br>
+  <input type="email" name="email">
+  <br>
+  password:<br>
+  <input type="password" name="password">
+  <br><br>
+  <input type="submit" value="Login">
+  </form>
+</div>
+</div>
+
+<!-- end login forom -->
+
+
+
+
+		<section class="container-full footer-wrapper">
+			<footer class="container">
+				<div class="row">
+					<div class="col-ms-6 col-sm-3 col-md-3">
+						<p class="yellow text-uppercase" role="heading">Contacts</p>
+
+<address><strong>Fort Stewart</strong><br>
+<a href="tel:555-555-1212">912-876-1715</a>
+
+<p><a href="mailto:email@domain.com">Director@fsyca.org</a></p>
+</address>
+
+<address><strong>Fort Gordon</strong><br>
+<a href="tel:555-555-1212">706-823-8000</a>
+
+<p><a href="mailto:fgycamail@fgyca.org">fgycamail@fgyca.org</a></p>
+
+<p><strong>Milledgeville</strong><br>
+<a href="tel:555-555-1212">478-445-0100</a></p>
+
+<p><a href="mailto:email@domain.com">info@mvyca.org</a></p>
+</address>
+
+<address>&nbsp;</address>
+
+						<ul class="list-inline social-media">
+							<li><a href="https://www.facebook.com/GeorgiaYCA/" target="_blank"><span class="fa fa-facebook" aria-hidden="true"></span><span class="sr-only">Facebook</span></a></li>
+							<li><a href="https://twitter.com/georgiayca" target="_blank"><span class="fa fa-twitter" aria-hidden="true"></span><span class="sr-only">Twitter</span></a></li>
+							<li><a href="https://www.instagram.com/georgiayca/" target="_blank"><span class="fa fa-instagram" aria-hidden="true"></span><span class="sr-only">Instagram</span></a></li>
+						</ul>
+					</div>
+					<div class="hidden-ms col-sm-4 col-md-3">
+						<p class="yellow text-uppercase" role="heading">QUICK LINKS</p>
+
+<ul class="list-unstyled">
+	<li><a href="/admissions/faqs.cms">Frequently Asked Questions</a></li>
+	<li><a href="/about-us/mission-and-values.cms">Mission and Values</a></li>
+	<li><a href="/admissions/application.cms">Cadet Application</a></li>
+	<li><a href="/admissions/required-documents.cms">Required Documents</a></li>
+	<li><a href="/academies/programs-in-georgia.cms">Programs in Georgia</a></li>
+	<li>&nbsp;</li>
+</ul>
+
+					</div>
+					<div class="col-ms-6 col-sm-5 col-md-3">
+						<p class="yellow text-uppercase" role="heading">Upcoming Events</p>
+						<ul class="list-unstyled events margin-bottom-40">
+							
+							<li>
+								<div class="date">
+									<div class="month text-uppercase">Apr</div>
+									<div class="day">18</div>
+								</div>
+								<p><a href=" /about-us/calendar-events-news.cms?date=2019-04-18&amp;view=day&amp;news_id=26&amp;recurrence_id=359">Youth ChalleNGe Program Information Session-Augusta</a></p>
+								<ul class="small list-unstyled">
+									<li>5:45 PM -6:30 PM (Check in begins at 5:00 pm)</li>
+									<li>Augusta Richmond County Library</li>
+								</ul>
+							</li>
+							
+							<li>
+								<div class="date">
+									<div class="month text-uppercase">Apr</div>
+									<div class="day">30</div>
+								</div>
+								<p><a href=" /about-us/calendar-events-news.cms?date=2019-04-30&amp;view=day&amp;news_id=25&amp;recurrence_id=352">Youth ChalleNGe Program Information Session - Columbus</a></p>
+								<ul class="small list-unstyled">
+									<li>5:00 PM-6:30 pm (Check begins at 4:30pm)</li>
+									<li>Columbus Public Library</li>
+								</ul>
+							</li>
+							
+						</ul>
+						<hr>
+						<p class="text-right"><a href="/about-us/calendar-events-news.cms">More Events</a></p>
+					</div>
+					<div class="hidden-ms hidden-sm col-md-3">
+						<p class="yellow text-uppercase" role="heading">Follow Us</p>
+						<div class="twitter">
+							<script async="" src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+							<iframe id="twitter-widget-0" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" class="twitter-timeline twitter-timeline-rendered" data-widget-id="profile:georgiayca" title="Twitter Timeline" style="position: static; visibility: visible; display: inline-block; width: 100%; padding: 0px; border: none; max-width: 100%; min-width: 180px; margin-top: 0px; margin-bottom: 0px; height: 379.90625px;"></iframe>
+						</div>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-sm-8">
+						<ul class="list-inline text-uppercase small">
+							<li>© 2019 Georgia Youth Challenge</li>
+							<li><a href="/site-map.cms">Site Map</a></li>
+						</ul>
+					</div>
+					<div class="col-sm-4">
+						<ul class="list-inline text-uppercase small">
+							<li>Web Design By: <a href="http://www.thirdwavedigital.com" target="_blank">Third Wave Digital</a></li>
+						</ul>
+					</div>
+				</div>
+			</footer>
+		</section><!--/ .footer-wrapper-->
+
+	<!-- / main-->
+
+
+
+</main><iframe scrolling="no" frameborder="0" allowtransparency="true" src="https://platform.twitter.com/widgets/widget_iframe.2e9f365dae390394eb8d923cba8c5b11.html?origin=https%3A%2F%2Fwww.georgiayouthchallenge.org&amp;settingsEndpoint=https%3A%2F%2Fsyndication.twitter.com%2Fsettings" title="Twitter settings iframe" style="display: none;"></iframe><iframe id="rufous-sandbox" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" title="Twitter analytics iframe" style="position: absolute; visibility: hidden; display: none; width: 0px; height: 0px; padding: 0px; border: none;"></iframe></body></html>
